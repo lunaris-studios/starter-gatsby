e6c6f885c19f5d69c7ca335445bfa986e112c600
Sam Craig <contact@samcraig.io>
2020-08-20 14:02:08 -0400

    refactor: replace testWrapper() HOC with direct render via @testing-library/react

--- src/components/foo/foo.test.tsx
+++ src/components/foo/foo.test.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as Testing from "@testing-library/react";
 
 import * as Util from "~/util";
-import * as Gatsby from "~/gatsby";
 
 import * as Component from "./foo";
 import * as Styled from "./foo.styled";
@@ -12,7 +11,7 @@ import { find } from "styled-components/test-utils";
 
 describe("Foo", () => {
 	it("renders correctly", () => {
-		const node = Util.testWrapper(<Component.Foo />);
+		const node = Testing.render(<Component.Foo />);
 		const FooContainer = find(node.baseElement, Styled.Foo.Container);
 
 		expect(FooContainer).toMatchSnapshot();
